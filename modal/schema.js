3f133488542c87daee1caf37301ca63c874f87fb
dev <dev@dev.com>
2023-01-19 12:12:09 +0530

    first commit

---
+++ modal/schema.js
@@ -0,0 +1,36 @@
+require('../db/db');
+const mongoose = require("mongoose");
+
+
+const dataschema = new mongoose.Schema({
+
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    mobile   : {
+        type: Number,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+      
+    }
+  
+})
+
+
+
+const data = new mongoose.model('data', dataschema);
+module.exports = data
